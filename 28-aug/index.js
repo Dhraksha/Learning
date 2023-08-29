5e9040bbc0615abcf178624acf1af47ad13689c8
Dhraksha <139321906+Dhraksha@users.noreply.github.com>
2023-08-29 11:35:17 +0530

    Delete 28-aug directory

--- 28-aug/index.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('drop-zone-input').forEach(inputElement => {
-   const dropZoneElement = inputElement.closest('.drop-zone');
-   
-   dropZoneElement.addEventListener('click' , function(){
-    inputElement.click();
-   })
-})
